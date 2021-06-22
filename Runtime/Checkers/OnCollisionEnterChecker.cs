b93e0dd7dbd8574016ea9e5c57203cbd62528e02
Arthur <37938234+supremestranger@users.noreply.github.com>
2021-06-22 17:12:57 +0300

    Update OnCollisionEnterChecker.cs

--- Runtime/Checkers/OnCollisionEnterChecker.cs
+++ Runtime/Checkers/OnCollisionEnterChecker.cs
@@ -6,7 +6,7 @@ namespace LeoEcsPhysics
     {
         private void OnCollisionEnter(Collision other)
         {
-            EcsPhysicsEventsEmitter.RegisterCollisionStayEvent(gameObject, other.collider, other.GetContact(0), other.relativeVelocity);
+            EcsPhysicsEventsEmitter.RegisterCollisionEnterEvent(gameObject, other.collider, other.GetContact(0), other.relativeVelocity);
         }
     }
 }
